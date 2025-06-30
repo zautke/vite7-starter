f6f68b70832ad4f96c47b8db450f35d2ee63686d
largo <largo@lukezautke.com>
2025-06-29 22:43:47 -0600

    Update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+import Terminal from "vite-plugin-terminal";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    cloudflare(),
+    Terminal({
+      console: 'terminal',
+      output: ['terminal', 'console']
+    })
+  ],
 })
